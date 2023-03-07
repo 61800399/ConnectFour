d628fd6e4e2402e58c8d7a565341d88c5c11c62d
61800399ctc <61800399@student.muskegonisd.org>
2023-03-07 14:08:33 -0500

    Add project files.

---
+++ Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Board
+{
+    public List<List<string>> Grid { get; set; }
+
+    public Board()
+	{
+		Initilize();
+    }
+	private void Initilize()
+	{
+		int g = 0;
+		Grid = new List<List<string>>();
+		for (int y = 0; y < 6; y++)
+		{
+			List<string> X = new List<string>();
+			for (int x = 0; x < 7; x++)
+			{
+				g++;
+				X.Add(g.ToString());
+			}
+			Grid.Add(X);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConnectFour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Board game = new Board();
+            List<List<string>> list = game.Grid;
+            output(list);
+        }
+        public static void output(List<List<string>> board)
+        {
+            foreach (var item in board)
+            {
+                string print = string.Join(", ", item);
+                Console.WriteLine(print);
+            }
+        }
+
+    }
+}
